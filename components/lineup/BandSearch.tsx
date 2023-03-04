4b4f2c0eb2d60cb242aa722f952633f4c75d2877
Daniel Büchele <daniel@buechele.cc>
2023-03-04 17:49:43 +0000

    tomo

--- components/lineup/BandSearch.tsx
+++ components/lineup/BandSearch.tsx
@@ -1,208 +1,105 @@
 import {gql, useApolloClient} from '@apollo/client';
-import {Input} from '@chakra-ui/react';
-import React, {useEffect, useMemo, useState} from 'react';
+import {Box, Input, Menu, MenuItem, Spinner, Stack} from '@chakra-ui/react';
+import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
 import {BandSerachQuery} from '../../types/graphql';
-
-class Typeahead<T> extends EventTarget {
-  private requests: Map<string, Promise<void>> = new Map();
-  private displaySets: Map<string, Map<string, T>> = new Map();
-  private currentQuery = '';
-
-  private fetcher: (query: string) => Promise<T[]>;
-  private keyExtractor: (data: T) => string = (data) => data.id;
-  private matchStringExtractor: (data: T) => string = (data) => data.name;
-  private debounce: number;
-  private displaySetLimit = 5;
-  private minimumQueryLength = 2;
-
-  constructor(fetcher: (query: string) => Promise<T[]>, debounce?: number) {
-    super();
-    this.fetcher = fetcher;
-    this.debounce = debounce ?? 150;
-  }
-
-  private matchesQuery(item: T, query: string): boolean {
-    const matchString = this.matchStringExtractor(item).toLocaleLowerCase();
-    return (
-      matchString.startsWith(query) || matchString.indexOf(` ${query}`) > -1
-    );
-  }
-
-  setQuery(q: string) {
-    const query = q.toLocaleLowerCase();
-
-    if (!query) {
-      this.displaySets.clear();
-      this.dispatchEvent(new CustomEvent<boolean>('loading', {detail: false}));
-      this.dispatchEvent(
-        new CustomEvent<T[]>('displaySetUpdated', {
-          detail: [],
-        }),
-      );
-      return;
-    }
-
-    if (this.displaySets.has(query)) {
-      this.dispatchEvent(new CustomEvent<boolean>('loading', {detail: false}));
-      this.dispatchEvent(
-        new CustomEvent<T[]>('displaySetUpdated', {
-          detail: Array.from(this.displaySets.get(query)?.values() ?? []),
-        }),
-      );
-      return;
-    }
-
-    // we don't have an existing display set, create a new one
-    const displaySet = new Map();
-
-    // maybe we can prefill it from a previous display set
-    const longestCommonDisplaySetKey = Array.from(this.displaySets.keys())
-      .filter((key) => query.startsWith(key))
-      .sort((a, b) => a.length - b.length)
-      .pop();
-
-    if (longestCommonDisplaySetKey != null) {
-      // append all results from previous display set that still match
-      const matchesFromPreviousSet = Array.from(
-        this.displaySets.get(longestCommonDisplaySetKey)?.values() ?? [],
-      ).filter((item) => this.matchesQuery(item, query));
-      this.appendToDisplaySet(displaySet, matchesFromPreviousSet);
-    }
-
-    this.displaySets.set(query, displaySet);
-    this.currentQuery = query;
-
-    // STEP 2: NETWORK
-    if (
-      this.requests.has(query) ||
-      displaySet.size >= this.displaySetLimit ||
-      query.length < this.minimumQueryLength
-    ) {
-      this.dispatchEvent(new CustomEvent<boolean>('loading', {detail: false}));
-      // query is already fetched or we have enough results we can bail early
-      return;
-    }
-
-    const req = async () => {
-      await new Promise((r) => setTimeout(r, this.debounce));
-      if (this.currentQuery !== query) {
-        // query has changed before debounce time elapsed, do nothing
-        return;
-      }
-
-      // send request to server
-      const data = await this.fetcher(query);
-      // artifically slow down network requests
-      await new Promise((r) => setTimeout(r, 2000));
-
-      if (this.currentQuery !== query && this.displaySets.has(query)) {
-        // query has changed, be we already made the request, so let's add it to the display set
-        // do not trigger an update, because it's not the current display set
-        this.appendToDisplaySet(this.displaySets.get(query)!, data, false);
-        return;
-      }
-
-      this.dispatchEvent(new CustomEvent<boolean>('loading', {detail: false}));
-      this.appendToDisplaySet(displaySet, data);
-    };
-
-    this.requests.set(
-      query,
-      req().then(() => {
-        this.requests.delete(query);
-      }),
-    );
-  }
-
-  appendToDisplaySet(
-    displaySet: Map<string, T>,
-    data: T[],
-    triggerUpdate?: false,
-  ) {
-    for (const item of data) {
-      const key = this.keyExtractor(item);
-      if (!displaySet.has(key)) {
-        displaySet.set(key, item);
-      }
-    }
-
-    if (triggerUpdate !== false) {
-      this.dispatchEvent(
-        new CustomEvent<T[]>('displaySetUpdated', {
-          detail: Array.from(displaySet.values()),
-        }),
-      );
-    }
-  }
-}
-
-function useTypeahead<T>(fetcher: (query: string) => Promise<T[]>): {
-  loading: boolean;
-  data: T[];
-  setQuery: (q: string) => void;
-} {
-  const ta = useMemo(() => new Typeahead(fetcher), [fetcher]);
-  const [data, setData] = useState<T[]>([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const cb1 = (e: Event) => setLoading((e as CustomEvent<boolean>).detail);
-    ta.addEventListener('loading', cb1);
-
-    const cb2 = (e: Event) => setData((e as CustomEvent<T[]>).detail);
-    ta.addEventListener('displaySetUpdated', cb2);
-
-    return () => {
-      ta.removeEventListener('loading', cb1);
-      ta.removeEventListener('displaySetUpdated', cb2);
-    };
-  }, [ta]);
-
-  return {
-    loading,
-    data,
-    setQuery: (q) => ta.setQuery(q),
-  };
-}
+import {TomoTypeaheadConfig, useTypeahead} from './TomoTypeahead';
 
 export default function BandSearch() {
   const client = useApolloClient();
-  const fetcher = useMemo(
-    () => (query: string) =>
-      client
-        .query<BandSerachQuery>({
-          query: gql`
-            query BandSerach($query: String!) {
-              findBandPlaying(query: $query) {
-                name
-                id
-                eventId
+
+  const config: TomoTypeaheadConfig<
+    BandSerachQuery['findBandPlaying'][number]
+  > = useMemo(
+    () => ({
+      fetcher: (query: string) =>
+        client
+          .query<BandSerachQuery>({
+            query: gql`
+              query BandSerach($query: String!) {
+                findBandPlaying(query: $query) {
+                  name
+                  id
+                  eventId
+                }
               }
-            }
-          `,
-          variables: {
-            query,
-          },
-        })
-        .then((d) => d.data.findBandPlaying),
+            `,
+            variables: {
+              query,
+            },
+          })
+          .then((d) => d.data.findBandPlaying),
+      keyExtractor: (data) => data.id,
+      matchStringExtractor: (data) => data.name.toLocaleLowerCase(),
+    }),
     [client],
   );
 
-  const {data, loading, setQuery} = useTypeahead(fetcher);
+  const [selected, setSelected] = useState(-1);
+  const [focused, setFocused] = useState(false);
+  const ref = useRef();
+
+  const {data, loading, setQuery} = useTypeahead(config);
+
+  const onKeyDown: React.KeyboardEventHandler<HTMLInputElement> = useCallback(
+    (e) => {
+      if (e.key == 'ArrowUp') {
+        e.preventDefault();
+        setSelected((s) => {
+          s--;
+          if (s < 0 && data.length > 0) {
+            s = data.length - 1;
+          }
+          return s;
+        });
+      } else if (e.key == 'ArrowDown') {
+        e.preventDefault();
+        setSelected((s) => {
+          s++;
+          if (s >= data.length) {
+            s = 0;
+          }
+          return s;
+        });
+      } else if (e.key == 'ArrowUp') {
+        e.preventDefault();
+      } else if (e.key == 'ArrowUp') {
+        e.preventDefault();
+      }
+    },
+    [],
+  );
+
+  useEffect(() => {
+    const item = data[selected];
+    if (item != null) {
+      // ref.current.value =
+    }
+  }, [ref, selected]);
+
+  console.log(loading);
 
   return (
-    <>
-      <Input
-        placeholder="Bands suchen…"
-        onChange={async (e) => setQuery(e.target.value)}
-      />
-      <ol>
-        {data.map((d) => (
-          <li key={d.id}>
-            {d.name} ({d.eventId.replace(/[^\d]/g, '')})
-          </li>
-        ))}
-      </ol>
-    </>
+    <Box
+      onFocus={() => setFocused(true)}
+      onBlur={() => setFocused(false)}
+      position="relative"
+    >
+      <Menu isOpen={data.length > 0 && focused} computePositionOnMount={true}>
+        <Input
+          ref={ref}
+          placeholder="Bands suchen…"
+          onChange={async (e) => setQuery(e.target.value)}
+          onKeyDown={onKeyDown}
+        />
+        <Stack position="absolute" bg="Background">
+          {data.map((d, i) => (
+            <MenuItem key={d.id}>
+              {d.name} ({d.eventId.replace(/[^\d]/g, '')})
+            </MenuItem>
+          ))}
+          {loading && <Spinner />}
+        </Stack>
+      </Menu>
+    </Box>
   );
 }
---
+++ components/lineup/TomoTypeahead.ts
@@ -0,0 +1,250 @@
+import {useMemo, useState, useEffect} from 'react';
+
+class TomoTypeaheadLoadingStateChangeEvent extends Event {
+  static readonly eventType = 'tomo-typeahead-loading';
+  readonly loading: boolean;
+
+  constructor(loading: boolean) {
+    super(TomoTypeaheadLoadingStateChangeEvent.eventType);
+    this.loading = loading;
+  }
+}
+
+class TomoTypeaheadDisplaySetUpdateEvent<T> extends Event {
+  static readonly eventType = 'tomo-typeahead-display-set-update';
+  readonly data: T[];
+
+  constructor(data: T[]) {
+    super(TomoTypeaheadDisplaySetUpdateEvent.eventType);
+    this.data = data;
+  }
+}
+
+export type TomoTypeaheadConfig<T> = {
+  fetcher: (query: string) => Promise<T[]>;
+  keyExtractor: (data: T) => string;
+  matchStringExtractor: (data: T) => string;
+  debounce?: number;
+  displaySetLimit?: number;
+  minimumQueryLength?: number;
+};
+
+class TomoTypeahead<T> extends EventTarget {
+  private requests: Map<string, Promise<void> | T[]> = new Map();
+  private displaySets: Map<string, Map<string, T>> = new Map();
+  private currentQuery = '';
+
+  private fetcher: (query: string) => Promise<T[]>;
+  private keyExtractor: (data: T) => string;
+  private matchStringExtractor: (data: T) => string;
+  private debounce = 150;
+  private displaySetLimit = 5;
+  private minimumQueryLength = 2;
+
+  constructor(config: TomoTypeaheadConfig<T>) {
+    super();
+    this.fetcher = config.fetcher;
+    this.keyExtractor = config.keyExtractor;
+    this.matchStringExtractor = config.matchStringExtractor;
+    this.debounce = config.debounce ?? this.debounce;
+    this.displaySetLimit = config.displaySetLimit ?? this.displaySetLimit;
+    this.minimumQueryLength =
+      config.minimumQueryLength ?? this.minimumQueryLength;
+  }
+
+  private matchesQuery(item: T, query: string): boolean {
+    const matchString = this.matchStringExtractor(item).toLocaleLowerCase();
+    return (
+      matchString.startsWith(query) || matchString.indexOf(` ${query}`) > -1
+    );
+  }
+
+  longestCommonPrefix<T>(query: string, map: Map<string, T>) {
+    const key = Array.from(map.keys())
+      .filter((key) => query.startsWith(key))
+      .sort((a, b) => a.length - b.length)
+      .pop();
+
+    if (key != null) {
+      return map.get(key);
+    }
+  }
+
+  setQuery(q: string) {
+    const query = q.toLocaleLowerCase();
+
+    if (!query) {
+      this.displaySets.clear();
+      this.requests.clear();
+      this.dispatchEvent(new TomoTypeaheadLoadingStateChangeEvent(false));
+      this.dispatchEvent(new TomoTypeaheadDisplaySetUpdateEvent<T>([]));
+      return;
+    }
+
+    let displaySet: Map<string, T>;
+
+    if (this.displaySets.has(query)) {
+      // we have a display set, but maybe the we haven't done a network request
+      displaySet = this.displaySets.get(query) ?? new Map();
+
+      this.dispatchEvent(
+        new TomoTypeaheadDisplaySetUpdateEvent(
+          Array.from(displaySet.values() ?? []),
+        ),
+      );
+    } else {
+      // we don't have an existing display set, create a new one
+      displaySet = new Map();
+
+      const longestCommonPrefixDisplaySet = this.longestCommonPrefix(
+        query,
+        this.displaySets,
+      );
+
+      // maybe we can prefill it from a previous display set
+      if (longestCommonPrefixDisplaySet != null) {
+        // append all results from previous display set that still match
+        const matchesFromPreviousSet = Array.from(
+          longestCommonPrefixDisplaySet?.values() ?? [],
+        ).filter((item) => this.matchesQuery(item, query));
+        this.appendToDisplaySet(displaySet, matchesFromPreviousSet);
+      }
+    }
+
+    this.currentQuery = query;
+    this.displaySets.set(query, displaySet);
+
+    const longestCommonPrefixRequest = this.longestCommonPrefix(
+      query,
+      this.requests,
+    );
+
+    // STEP 2: NETWORK
+    if (
+      query.length < this.minimumQueryLength ||
+      // request is already in flight
+      this.requests.has(query) ||
+      // we already have enough local results
+      displaySet.size >= this.displaySetLimit ||
+      // LCP request already entirely included, nothing more to fetch
+      (Array.isArray(longestCommonPrefixRequest) &&
+        longestCommonPrefixRequest.every((r) =>
+          displaySet.has(this.keyExtractor(r)),
+        ))
+    ) {
+      this.dispatchEvent(new TomoTypeaheadLoadingStateChangeEvent(false));
+      // query is already fetched or we have enough results we can bail early
+      return;
+    }
+
+    const req = async () => {
+      this.dispatchEvent(new TomoTypeaheadLoadingStateChangeEvent(true));
+      await new Promise((r) => setTimeout(r, this.debounce));
+
+      if (this.currentQuery !== query) {
+        // query has changed before debounce time elapsed, do nothing
+        return;
+      }
+
+      // send request to server
+      const data = await this.fetcher(query);
+
+      // artifically slow down network requests for debugging
+      // await new Promise((r) => setTimeout(r, 2000));
+
+      if (this.currentQuery !== query) {
+        // query has changed, be we already made the request, so let's add it to the display set
+        // do not trigger an update, because it's not the current display set
+        this.appendToDisplaySet(displaySet, data, false);
+      } else {
+        this.dispatchEvent(new TomoTypeaheadLoadingStateChangeEvent(false));
+        this.appendToDisplaySet(displaySet, data);
+      }
+
+      return data;
+    };
+
+    this.requests.set(
+      query,
+      req().then((data) => {
+        if (data != null) {
+          this.requests.set(query, data);
+        }
+      }),
+    );
+  }
+
+  appendToDisplaySet(
+    displaySet: Map<string, T>,
+    data: T[],
+    triggerUpdate?: false,
+  ) {
+    for (const item of data) {
+      const key = this.keyExtractor(item);
+      if (!displaySet.has(key)) {
+        displaySet.set(key, item);
+      }
+    }
+
+    if (triggerUpdate !== false) {
+      this.dispatchEvent(
+        new TomoTypeaheadDisplaySetUpdateEvent<T>(
+          Array.from(displaySet.values()),
+        ),
+      );
+    }
+  }
+}
+
+export function useTypeahead<T>(config: TomoTypeaheadConfig<T>): {
+  loading: boolean;
+  data: T[];
+  setQuery: (q: string) => void;
+} {
+  const ta = useMemo(() => new TomoTypeahead(config), [config]);
+  const [data, setData] = useState<T[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const loadingListener = (e: Event) => {
+      if (!(e instanceof TomoTypeaheadLoadingStateChangeEvent)) {
+        return;
+      }
+      setLoading(e.loading);
+    };
+
+    const dataListener = (e: Event) => {
+      if (!(e instanceof TomoTypeaheadDisplaySetUpdateEvent)) {
+        return;
+      }
+      setData(e.data);
+    };
+
+    ta.addEventListener(
+      TomoTypeaheadLoadingStateChangeEvent.eventType,
+      loadingListener,
+    );
+
+    ta.addEventListener(
+      TomoTypeaheadDisplaySetUpdateEvent.eventType,
+      dataListener,
+    );
+
+    return () => {
+      ta.removeEventListener(
+        TomoTypeaheadLoadingStateChangeEvent.eventType,
+        loadingListener,
+      );
+      ta.removeEventListener(
+        TomoTypeaheadDisplaySetUpdateEvent.eventType,
+        dataListener,
+      );
+    };
+  }, [ta]);
+
+  return {
+    loading,
+    data,
+    setQuery: (q) => ta.setQuery(q),
+  };
+}
